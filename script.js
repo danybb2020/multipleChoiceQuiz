b134f4512821e92d546afd0ef3cbf7cb663ba0d5
danybb2020 <daniel.balderas@gmail.com>
2020-07-07 17:51:20 -0700

    fixed issues with html, css and JS

--- script.js
+++ script.js
@@ -1,43 +1,65 @@
 var startButton = document.getElementById("start-btn");
-var questionTime = 60
-var clock = document.querySelector("#clock");
+var questionTime = 60;
+var penaltyTime = 5;
+var clock = document.getElementById("clock");
 var timer ;
 var changingQuestions = document.getElementById("question-container");
 var changingAnswers = document.getElementById("answer-buttons");
 var currentIndex =  0;
 
 
+
 function startQuiz (){
    console.log('Started');
-   Startbutton.classList.add('hide');
-   questionContainerElement.classList.remove('hide');
-   timer = setInterval (startTime, 1000);
-   setNextQuestion()
+   startButton.classList.add('hide');
+   changingQuestions.classList.remove('hide');
+   timer = setInterval(startTime, 1000);
+   setNextQuestion();
 
 }
 
 function setNextQuestion(){
    var currentQuestion = questions [currentIndex];
    changingQuestions.textContent=currentQuestion.question;
-   currentQuestions.answers.array.forEach(function(choice, i) {
-     var newBtn= document.createElement("button");
-newBtn.setAttribute ("value", choice);
-newBtn.onclick= selectAnswer;
-changingAnswers.appendChild(newBtn)
+   currentQuestion.answers.forEach(function(choice, i) {
+      var newBtn= document.createElement("button");
+      newBtn.setAttribute ("value", choice);
+      newBtn.textContent =  choice;
+      newBtn.onclick = selectAnswer;
+      changingAnswers.appendChild(newBtn)
    });
 }
 
 function selectAnswer (){
-///text value of the button is the same as your correct answer 
-///corrent index goes up one 
-currentIndex ++;
-setNextQuestion();
+  ///text value of the button is the same as your correct answer 
+  console.log(this.getAttribute("value"));
+  console.log(questions[currentIndex].correctAnswer );
+  if (this.getAttribute("value") == questions[currentIndex].correctAnswer +'' ) {
+    
+    // get rid of old questions
+    var oldButtonsArr = document.querySelectorAll('#answer-buttons button');
+    oldButtonsArr.forEach(function(btn,i) {
+      btn.remove();
+    });
+
+    ///corrent index goes up one 
+    currentIndex ++;
+    setNextQuestion();
+
+  } else {
+  questionTime = questionTime-penaltyTime;
+  this.setAttribute("disabled", "disabled");
+  this.textContent =  "nope";
+
+
+    
+  }
 
 }
 
 function stopGame (){
 clearInterval(timer);
-
+console.log ("End of quiz");
 }
 
 function startTime () {
@@ -74,7 +96,7 @@ const questions = [
     question: "What is not true of an array?",
     answers: [
       "Answer1: Is is object lets you store multiple values in a single variable ", "Answer 2: It stores a fixed-size sequential collection of elements of the same type." , "Answer3: An array is used to store a collection of data,", "Answer4: It is not a collection of variables of the same type"
-      he Array .    
+       
     ],
     correctAnswer: "Answer4: It is not a collection of variables of the same type"
   },
